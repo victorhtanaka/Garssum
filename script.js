d67f4d47e54ecdad74072a6e71fba133dca17aa2
victorhtanaka <victorhtanaka555@gmail.com>
2023-10-26 19:50:29 -0300

    top 10 formularios gratis spacex clone ela muschen

--- script.js
+++ script.js
@@ -45,3 +45,16 @@ function updateProgressbar() {
   progress.style.width =
     ((progressActive.length - 1) / (progressSteps.length - 1)) * 100 + "%";
 }
+
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
