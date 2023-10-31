fc006f4512f40ffadf83fd55b0303ee168bb854c
victorhtanaka <victorhtanaka555@gmail.com>
2023-10-31 20:10:39 -0300

    form pronto praticamente

--- form.js
+++ form.js
@@ -10,9 +10,9 @@ function showTab(n) {
     document.getElementById("prevBtn").style.display = "inline";
   }
   if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";
+    document.getElementById("nextBtn").innerHTML = "Finalizar";
   } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
+    document.getElementById("nextBtn").innerHTML = "Próximo";
   }
   fixStepIndicator(n)
 }
