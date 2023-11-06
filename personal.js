32f93929d5b1da7abeba3f36cfa8c9187368f03e
victorhtanaka <victorhtanaka555@gmail.com>
2023-11-05 21:32:25 -0300

    HAHAHAHAHAHAHAH POSITION VOCE FOI DERROTADO

--- personal.js
+++ personal.js
@@ -3,6 +3,11 @@ var currentop = 1
 function changeop(n) {
     document.getElementById('p' + currentop).style.color = '#B2B2B2';
     let numero = n.replace(/p/, '');
+    document.getElementById('rightdiv' + currentop).style.display = 'none';
+    let newpage = n.replace(/p/, 'rightdiv');
+    document.getElementById(newpage).style.display = 'flex';
     currentop = parseInt(numero);
     document.getElementById(n).style.color = '#26DD9B';
+    console.log(n)
+    console.log(numero)
 }
