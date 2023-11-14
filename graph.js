a8816b59fdaae5ecab649738c32ad123ded625b0
victorhtanaka <victorhtanaka555@gmail.com>
2023-11-14 10:19:46 -0300

    grafifos

---
+++ graph.js
@@ -0,0 +1,24 @@
+const xValues = [100,200,300,400,500,600,700,800,900,1000];
+        
+new Chart("myChart", {
+    type: "line",
+    data: {
+    labels: xValues,
+    datasets: [{ 
+        data: [860,1140,1060,1060,1070,1110,1330,2210,7830,2478],
+        borderColor: "#3DD598",
+        fill: false
+    }, { 
+        data: [1600,1700,1700,1900,2000,2700,4000,5000,6000,7000],
+        borderColor: "#74EE9B",
+        fill: false
+    }, { 
+        data: [8000, 7700,7800, 7500, 7000, 7200, 7300, 7000, 6500, 6000],
+        borderColor: "#84ECBF",
+        fill: false
+    }]
+    },
+    options: {
+    legend: {display: false}
+    }
+});
